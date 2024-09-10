8041c69384edb2320e811935d1f4c588961e6c9b
nicolasallp <nikolasalp@gmail.com>
2024-09-09 23:38:09 -0300

    add: script for edit todo

--- mvc-todo/wwwroot/js/site.js
+++ mvc-todo/wwwroot/js/site.js
@@ -1,4 +1,21 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const editBtns = document.querySelectorAll(".edit-btn")
+const todoTitle = 
 
-// Write your JavaScript code.
+editBtns.forEach((element) => {
+    element.addEventListener("click", (event) => {
+        let parent = event.currentTarget.parentElement
+        let todoTitle = parent.previousElementSibling
+        let gParent = parent.parentElement
+
+        element.classList.remove("hover:bg-opacity-10")
+        element.classList.add("bg-white")
+        element.innerHTML = `<img src="icons/check.png" class="size-6" />`
+
+
+        gParent.style.backgroundColor = "#1a294a"
+        todoTitle.innerHTML = `<input value=${todoTitle.textContent} class="bg-[#1a294a] border-0 outline-0 rounded w-full text-[18px] text-gray-200 placeholder-opacity-50 font-segoe" type="text" />`
+        let input = todoTitle.firstElementChild
+        input.focus();
+        input.select();
+    })
+})
