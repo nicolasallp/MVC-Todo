f01118f782d8daa3277107991f9683aac9fa0715
nicolasallp <nikolasalp@gmail.com>
2024-09-10 17:27:39 -0300

    update: script

--- mvc-todo/wwwroot/js/site.js
+++ mvc-todo/wwwroot/js/site.js
@@ -1,21 +1,36 @@
 ﻿const editBtns = document.querySelectorAll(".edit-btn")
-const todoTitle = 
 
 editBtns.forEach((element) => {
     element.addEventListener("click", (event) => {
+        editBtns.forEach((element1) => {
+            let parent1 = element1.parentElement
+            let todoTitle1 = parent1.previousElementSibling
+            let gParent1 = parent1.parentElement
+            let todoIn1 = todoTitle1.firstElementChild
+
+            if (gParent1.style.backgroundColor == "rgb(26, 41, 74)") {
+                gParent1.style.backgroundColor = "#253d73"
+                element1.nextElementSibling.classList.add("hidden")
+                todoTitle1.innerHTML = todoIn1.value
+                todoIn1.classList.add("hidden")
+            }
+        })
         let parent = event.currentTarget.parentElement
         let todoTitle = parent.previousElementSibling
         let gParent = parent.parentElement
 
-        element.classList.remove("hover:bg-opacity-10")
-        element.classList.add("bg-white")
-        element.innerHTML = `<img src="icons/check.png" class="size-6" />`
-
+        element.nextElementSibling.classList.remove("hidden")
 
         gParent.style.backgroundColor = "#1a294a"
-        todoTitle.innerHTML = `<input value=${todoTitle.textContent} class="bg-[#1a294a] border-0 outline-0 rounded w-full text-[18px] text-gray-200 placeholder-opacity-50 font-segoe" type="text" />`
-        let input = todoTitle.firstElementChild
-        input.focus();
-        input.select();
+        todoTitle.childNodes.forEach((node) => {
+            if (node.nodeType === Node.TEXT_NODE) {
+                todoTitle.removeChild(node)
+            }
+        })
+
+        todoIn = todoTitle.firstElementChild
+        todoIn.classList.remove("hidden")
+        todoIn.focus();
+        todoIn.select();
     })
 })
